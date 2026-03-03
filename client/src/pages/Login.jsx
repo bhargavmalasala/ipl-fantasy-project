7290584f8cd58a6dd1930f48894dd2b626411c0c
bhargavmalasala <tzbhargav@gmail.com>
2026-03-03 20:08:32 +0530

    Add routing and authentication functionality with Login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const res = await api.post("auth/login", { email, password });
+            localStorage.setItem("token", res.data.token);
+            navigate("/admin");
+        } catch (error) {
+            alert("Login failed. Please check your credentials.");
+        }
+    }
+    return (
+        <div>
+      <h2>Admin Login</h2>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+    );
+}
